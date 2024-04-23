aad8edd5b160545d45c18d23aed52c7b431ab189
Zam <samuel.kodehode@gmail.com>
2024-04-23 13:59:26 +0200

    added login and new user logic

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -9,14 +9,22 @@ const props = defineProps< {
 </script>
 
 <template>
-<div>
-    <h1>{{ props.book.title }}</h1>
+<div id="book-wrapper">
+    <h3>{{ props.book.title }}</h3>
     <h6>{{ props.book.id }}</h6>
-    <h1>{{ props.book.author }}</h1>
-    <h1>{{ props.book.year }}</h1>
+    <h3>{{ props.book.author }}</h3>
+    <h3>{{ props.book.year }}</h3>
 </div>
 </template>
 
 <style scoped>
-
+#book-wrapper{
+    border: 1px solid black;
+    margin: 10px;
+    padding: 10px;
+    width: 500px;
+    display: inline-block;
+    text-align: center;
+    background-color: #545454;
+}
 </style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRouter } from 'vue-router'
+import {ref} from 'vue';
+import {useRouter} from 'vue-router'
 
 const router = useRouter()
 const username = ref('');
 const password = ref('');
+const newEmail = ref('');
+const newUsername = ref('');
+const newPassword = ref('');
+const newUserPrompt = ref('');
+const newUserError = ref('');
 const error = ref('');
+const showNewUserForm = ref(false);
 
 const login = async () => {
     try {
@@ -34,22 +40,82 @@ const login = async () => {
         error.value = err.message;
     }
 };
+
+const postNewUser = async () => {
+    try {
+        const response = await fetch('http://localhost:5000/users', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: newUsername.value,
+                email: newEmail.value,
+                password: newPassword.value
+            })
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log('New user created:', data);
+            newEmail.value = '';
+            newUsername.value = '';
+            newPassword.value = '';
+            newUserPrompt.value = 'New user created successfully';
+        } else {
+            console.error('Failed to create new user:', response.statusText);
+            newUserError.value = response.statusText;
+        }
+    } catch (err: any) {
+        console.error('Error creating new user:', err.message);
+        newUserError.value = err.message;
+    }
+};
+
+const handleNewUser = () => {
+    showNewUserForm.value = !showNewUserForm.value;
+};
+
 </script>
 
 <template>
     <div>
         <h1>Login</h1>
         <form @submit.prevent="login">
-            <input type="text" placeholder="Username" v-model="username" />
-            <input type="password" placeholder="Password" v-model="password" />
+            <input type="text" placeholder="Username" v-model="username"/>
+            <input type="password" placeholder="Password" v-model="password"/>
             <button type="submit">Login</button>
         </form>
         <p v-if="error" style="color: red;">{{ error }}</p>
     </div>
+    <div>
+        <h5 id="new-user-text" @click="handleNewUser">New User</h5>
+        <div v-if="showNewUserForm">
+            <form @submit.prevent="postNewUser" id="new-user-form">
+                <input type="email" placeholder="email" v-model="newEmail"/>
+                <input type="text" placeholder="Username" v-model="newUsername"/>
+                <input type="password" placeholder="Password" v-model="newPassword"/>
+                <button type="submit">New User</button>
+            </form>
+            <p v-if="error" style="color: red;">{{ newUserError }}</p>
+            <p v-if="newUserPrompt" style="color: greenyellow;">{{ newUserPrompt }} </p>
+        </div>
+
+    </div>
 </template>
 
 
-
 <style scoped>
-/* Add scoped styles if needed */
+form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+#new-user-text {
+    cursor: pointer;
+    text-decoration: underline;
+}
+
 </style>
